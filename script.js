77d2bce6386c8e7481e1f4708461e3f995c39b5d
guigesaifarm <guigesaifarm@gmail.com>
2026-02-24 19:03:32 -0300

    Refactor changeVideo to playVideo with reset

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function changeVideo(videoId) {
-    // Pausar todos os vídeos
-    const videos = document.querySelectorAll('video');
-    videos.forEach(video => {
-        video.pause();
-    });
-    
-    // Reproduzir o vídeo selecionado
-    const videoPlayer = document.getElementById(videoId);
-    videoPlayer.play();
+function playVideo(videoId){
+
+const videos = document.querySelectorAll("video");
+
+videos.forEach(video => {
+video.pause();
+video.currentTime = 0;
+});
+
+const selected = document.getElementById(videoId);
+selected.play();
+
 }
